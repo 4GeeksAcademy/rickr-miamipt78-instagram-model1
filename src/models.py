d1f39da79f21f28b4aaf4eb9dbe4fab436df11c0
Rick Rodriguez <rickrod6502@gmail.com>
2025-10-28 00:59:57 +0000

    added users and posts classes

--- src/models.py
+++ src/models.py
@@ -1,19 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
 
+    # relationships
+    posts: Mapped[List["Post"]] = relationship()
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # relationships
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
         }
